2275dbadff3ba9647dd15219e41a6025b00f5413
florian-lg <legars.florian@gmail.com>
2019-08-22 00:42:16 +0200

    Send message to channel

--- commands/dbd/RandomPerks.js
+++ commands/dbd/RandomPerks.js
@@ -24,13 +24,13 @@ module.exports = class RandomPerks extends Command {
                 .setTitle((splittedMessage == 'survivor') ? '**SURVIVANT**' : '**TUEUR**')
                 .setAuthor(message.member.displayName, message.member.user.avatarURL)
                 .setColor((splittedMessage == 'survivor') ? 0x0c60fc : 0xde2937)
-                .setThumbnail((splittedMessage == 'survivor') ? 
-                    'https://66.media.tumblr.com/1ba3391fc0921c7ab004d1ff6b3a0904/tumblr_p30uex82gR1w1zx8lo1_1280.png' : 
-                    'https://img00.deviantart.net/b0f0/i/2017/344/3/1/myers_the_shape__dbd__by_rucchii-dbwaulq.png');
-                for(let i = 0; i < 4; i++) {
-                    embed.addField(`Perk n° ${i+1}`, perkArr[i].perk_name,  true)
-                }
-                message.channel.send(embed);
-        });
-    } 
+          .setThumbnail((splittedMessage == 'survivor')
+            ? 'https://66.media.tumblr.com/1ba3391fc0921c7ab004d1ff6b3a0904/tumblr_p30uex82gR1w1zx8lo1_1280.png'
+            : 'https://img00.deviantart.net/b0f0/i/2017/344/3/1/myers_the_shape__dbd__by_rucchii-dbwaulq.png');
+        for (let i = 0; i < 4; i++) {
+          embed.addField(`Perk n° ${i + 1}`, perkArr[i].perk_name, true);
+        }
+        message.channel.send(embed);
+      });
+  }
 }
--- commands/thiercelieux/SendRoles.js
+++ commands/thiercelieux/SendRoles.js
@@ -12,12 +12,12 @@ module.exports = class SendRoles extends Command {
         .then( function (response) {
             axios.get(`http://127.0.0.1:8080/api/game-players/${response.data[0].id}`)
             .then( function (response) {
-                response.data.forEach( player => {
+                response.data.forEach( player => {      
                     axios.post('http://127.0.0.1:8080/api/send-roles', ({
                         playerId : player.id
                     }))
                     .then( function (response) {
-                        console.log(response);
+                        message.channel.send("Les rôles on été ajoutés !")
                     })
                     .catch( function (error) {
                         console.log(error);
