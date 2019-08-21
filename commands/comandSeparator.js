0e507f9b08257a101b48d044d9fd602e80ae0d2f
florian-lg <legars.florian@gmail.com>
2019-08-22 00:43:22 +0200

    Remove funCommands.js

--- commands/comandSeparator.js
+++ commands/comandSeparator.js
@@ -1,5 +1,4 @@
 const UpdateRoles = require('./updateRoles');
-const FunCommands = require('./funCommands');
 const RandomPerks = require('./dbd/RandomPerks');
 const Game = require('./thiercelieux/Game');
 const RegisterPlayer = require('./thiercelieux/RegisterPlayer');
@@ -7,35 +6,31 @@ const SendRoles = require('./thiercelieux/SendRoles');
 
 module.exports = class CommandSeparator {
 
-    static identifyMessage(message) {
+  static identifyMessage(message) {
 
-        let messageReceived = message.content.split(' ')[0];
-        console.log(messageReceived);
-        switch (messageReceived) {
-            case '!join':
-                console.log(`funCommand used by ${message.member.displayName}`);
-                FunCommands.parse(message);             
-                break;
-            case '!update':
-                console.log(`UpdateRoles used by ${message.member.displayName}`);
-                UpdateRoles.parse(message);              
-                break;
-            case '!random':
-                console.log(`RandomPerks used by ${message.member.displayName}`);
-                RandomPerks.parse(message);
-                break;
-            case '!loupgarou':
-                console.log(`Game used by ${message.member.displayName}`);
-                Game.parse(message);
-                break;
-            case '!register':
-                console.log(`Register used by ${message.member.displayName}`);
-                RegisterPlayer.parse(message);
-                break;
-            case '!send-roles':
-                console.log(`SendRoles used by ${message.member.displayName}`);
-                SendRoles.parse(message);
-                break;
-        }
+    const messageReceived = message.content.split(' ')[0];
+    console.log(messageReceived);
+    switch (messageReceived) {
+      case '!update':
+        console.log(`UpdateRoles used by ${message.member.displayName}`);
+        UpdateRoles.parse(message);
+        break;
+      case '!random':
+        console.log(`RandomPerks used by ${message.member.displayName}`);
+        RandomPerks.parse(message);
+        break;
+      case '!loupgarou':
+        console.log(`Game used by ${message.member.displayName}`);
+        Game.parse(message);
+        break;
+      case '!register':
+        console.log(`Register used by ${message.member.displayName}`);
+        RegisterPlayer.parse(message);
+        break;
+      case '!send-roles':
+        console.log(`SendRoles used by ${message.member.displayName}`);
+        SendRoles.parse(message);
+        break;
     }
-}
+  }
+};
